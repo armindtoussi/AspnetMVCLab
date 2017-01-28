e3427a7c3a70f15daa1ca38a61a30ec499122829
drwatley <atoussi@my.bcit.ca>
2017-01-28 13:04:45 -0800

    first commit

---
+++ Controllers/FilesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab3_Stuff.Controllers
+{
+    public class FilesController : Controller
+    {
+        // GET: Files
+        public ActionResult Index()
+        {
+            string[] files = Directory.GetFiles(Server.MapPath("~/TextFiles"));
+            ViewBag.myFiles = files;
+
+            for(int i = 0; i < files.Length; i++)
+            {
+                files[i] = files[i].Remove(files[i].Length - 4, 4);
+                files[i] = files[i].Remove(0, files[i].Length - 5);
+            }
+
+            return View();
+        }
+
+        public ActionResult FileContent(string filename)
+        {
+            string contents = System.IO.File.ReadAllText(Server.MapPath("~/TextFiles/" + filename + ".txt"));
+            ViewBag.file = contents;
+            return View();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab3_Stuff.Startup))]
+namespace Lab3_Stuff
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
